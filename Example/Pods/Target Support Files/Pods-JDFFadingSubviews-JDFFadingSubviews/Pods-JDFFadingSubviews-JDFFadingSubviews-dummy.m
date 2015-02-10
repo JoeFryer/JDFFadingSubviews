ba44033351981b7c6186352e4f2c477328b0b83d
Joe Fryer <joe.d.fryer@gmail.com>
2015-02-10 17:59:40 +0000

    Project setup.

---
+++ Example/Pods/Target Support Files/Pods-JDFFadingSubviews-JDFFadingSubviews/Pods-JDFFadingSubviews-JDFFadingSubviews-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JDFFadingSubviews_JDFFadingSubviews : NSObject
+@end
+@implementation PodsDummy_Pods_JDFFadingSubviews_JDFFadingSubviews
+@end
